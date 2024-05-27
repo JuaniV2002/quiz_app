2026ab1d49082bdbc76ab71d59e8f3d7cd9d857f
JuaniV2002 <juani2002villa@gmail.com>
2024-05-27 20:59:04 -0300

    cupertino styled answer buttons

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,10 +1,11 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class Answer extends StatelessWidget {
   final VoidCallback selectHandler;
   final String answerText;
 
-  const Answer(this.selectHandler, this.answerText, {Key? key}) : super(key: key);
+  const Answer(this.selectHandler, this.answerText, {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,18 +13,16 @@ class Answer extends StatelessWidget {
       width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(10.0), // Add margin here
-        child: ElevatedButton(
-          style: ButtonStyle(
-            shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15),
-              ),
-            ),
-            backgroundColor: WidgetStateProperty.all<Color>(Colors.blue),
-            foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
-          ),
-          child: Text(answerText),
+        child: CupertinoButton(
+          color: CupertinoColors.activeBlue, // Button color
+          borderRadius: BorderRadius.circular(15), // Button border radius
           onPressed: selectHandler,
+          child: Text(
+            answerText,
+            style: const TextStyle(
+              fontSize: 20,
+            ),
+          ),
         ),
       ),
     );
