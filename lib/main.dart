a5bfdb8763cb4bd8314e888c6d0b0e002489b5c5
JuaniV2002 <juani2002villa@gmail.com>
2024-05-27 21:00:16 -0300

    nunito font added for the whole app

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, deprecated_member_use, avoid_print, no_logic_in_create_state, unused_element
 import 'package:first_app/result.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 import './quiz.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() {
     return _MyAppState();
@@ -17,7 +19,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
-      'questionText': 'Cual es mi apodo?',
+      'questionText': 'Cuál es mi apodo?',
       'answers': [
         {'text': 'Juan', 'score': 5},
         {'text': 'Juani', 'score': 10},
@@ -26,16 +28,16 @@ class _MyAppState extends State<MyApp> {
       ],
     },
     {
-      'questionText': 'Que estoy estudiando?',
+      'questionText': 'Qué estoy estudiando?',
       'answers': [
-        {'text': 'Computacion', 'score': 10},
-        {'text': 'Biologia', 'score': 5},
-        {'text': 'Administracion', 'score': 3},
+        {'text': 'Computación', 'score': 10},
+        {'text': 'Biología', 'score': 5},
+        {'text': 'Administración', 'score': 3},
         {'text': 'Nada', 'score': 0}
       ],
     },
     {
-      'questionText': 'Cual es el apodo preferido de mi novia?',
+      'questionText': 'Cuál es el apodo preferido de mi novia?',
       'answers': [
         {'text': 'Belu', 'score': 0},
         {'text': 'Belen', 'score': 3},
@@ -60,26 +62,37 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
-    print(_questionIndex);
-    if (_questionIndex < _questions.length) {
-      print('We have more questions!');
-    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Cuanto conoces a Juani?'),
+    return CupertinoApp(
+      theme: const CupertinoThemeData(
+        textTheme: CupertinoTextThemeData(
+          textStyle: TextStyle(
+            fontFamily: 'Nunito',
+          ),
         ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                answerQuestion: _answerQuestion,
-                questionIndex: _questionIndex,
-                questions: _questions,
-              )
-            : Result(_totalScore, _resetQuiz),
+      ),
+      home: CupertinoPageScaffold(
+        backgroundColor: const Color.fromARGB(255, 107, 156, 198),
+        navigationBar: const CupertinoNavigationBar(
+          middle: Text(
+            'Cuánto conoces a Juani?',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
+        child: SafeArea(
+          child: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questionIndex: _questionIndex,
+                  questions: _questions,
+                )
+              : Result(_totalScore, _resetQuiz),
+        ),
       ),
     );
   }
